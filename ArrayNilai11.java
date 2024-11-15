3be698c0a285140a585858642b0e3969cbc0489f
rifat syamlan <rifatsyamlan78@gmail.com>
2024-11-15 15:00:14 +0700

    commit

---
+++ ArrayNilai11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArrayNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++){
+            System.out.print("Masukkan Nilai Akhir ke "+i+" : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println("Nilai Akhir ke -"+i+" adalah "+nilaiAkhir[i]);
+        }
+    }
+}
